ff8aafaf56f2f2e149a625eabd6d4de2140542fd
MehediHasan128 <mehedihasan12926@gmail.com>
2023-11-06 17:33:49 +0530

    install jsonwebtoken

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +27,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send(token)
+    })
+
 
     const blogCollection = client.db('blogPage').collection('blogs');
     const wishListCollection = client.db('blogPage').collection('wishLists')
